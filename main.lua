255a4c42d08d630ad65695e670d384cc889b2983
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-08-26 23:37:06 -0500

    starting to figure out how ill add the game selection thing

--- main.lua
+++ main.lua
@@ -84,7 +84,7 @@ function love.load()
             layout={mode="horizontal",spacing=12}
     })
 
-    ui.control(0,0,80,5,panel2)
+    --ui.control(0,0,80,5,panel2)
 
     time=ui.text(0,0,"00:00",panel2,{
         align={x="left",y="center"},
@@ -126,6 +126,27 @@ function love.load()
         align={x="left",y="center"},
         margin={bottom=0,top=0,left=0,right=0},
         --class="icon"
+    })
+
+
+    ui.custom(0,0,ui.w,200,function(self)
+            --lg.rectangle("fill",self.x,self.y,self.w,self.h)
+            local s=128
+            local sb=192
+            local selected=0
+
+            for i=0,3 do
+                if i==selected then
+                    lg.rectangle("fill",i*(s+self.layout.spacing)+(self.w/2-sb/2),self.y+self.h-sb,sb,sb,10,10)
+                else
+                    lg.rectangle("fill",i*(s+self.layout.spacing)+(self.w/2-s/2),self.y+self.h-s,s,s,10,10)
+                end
+            end
+        end,control,{
+        align={x="center",y="bottom"},
+        margin={bottom=64,top=0,left=0,right=0},
+        padding={bottom=0,top=0,left=12,right=12},
+        layout={mode="horizontal",spacing=64}
     })
 
     testCanvas=lg.newCanvas(200,200)
